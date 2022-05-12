ba4bb186ab5e0739bae983219b640f20864ec04e
DanCristianIlie <ilie.dancrist@protonmail.com>
2022-05-12 20:16:46 +0300

    part 1 - final project blog

---
+++ src/views/Home.js
@@ -0,0 +1,18 @@
+import { Container, Row } from "reactstrap";
+
+function Home() {
+	return (
+		<Container>
+			<Row>
+				<div
+					style={{
+						height: "100vh",
+						width: "100%",
+						backgroundColor: "gray",
+					}}></div>
+			</Row>
+		</Container>
+	);
+}
+
+export default Home;
